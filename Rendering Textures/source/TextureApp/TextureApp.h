6ebd374dae05484e9481050f58f2ba38adb6e470
Urias Rooney <Godofdrakes@gmail.com>
2015-09-11 10:49:08 -0700

    Finished rendering of texture.

--- Rendering Textures/source/TextureApp/TextureApp.h
+++ Rendering Textures/source/TextureApp/TextureApp.h
@@ -3,11 +3,18 @@
 #define _TextureApp_H_
 
 #include <Application.h>
+class Camera;
 
 class TextureApp : public Application {
-    unsigned int texture;
+    unsigned int vao, vbo, ibo, programID, textureID;
+    Camera* camera;
 protected:
+    bool BuildShaders(const char* pathToVertexShader, const char* pathToFragmentShader);
+    unsigned int LoadShader(const char* pathToShader, unsigned int shaderType);
     virtual void AfterInit() override;
+    virtual void BeforeUpdate() override;
+    virtual void BeforeRender() override;
+    virtual void AfterRender() override;
 public:
     TextureApp();
     TextureApp( const std::string set_name );
